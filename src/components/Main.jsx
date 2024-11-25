19e83c09cd086f8986423539ee7b76e4faba91e4
TreasureUzoma <treasureuzoma650@gmail.com>
2024-11-25 10:17:46 +0100

    ui: fixed dark btn

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -55,7 +55,7 @@ const Main = () => {
                     >
                         <a
                             target="_blank"
-                            className="bg-[#525454] text-white px-3 py-2 text-[0.9rem] font-semibold rounded-lg border border-1 border-[#525454] hover:bg-white text-[#525454]"
+                            className="bg-[#525454] text-white px-3 py-2 text-[0.9rem] font-semibold rounded-lg border border-1 border-[#525454] hover:bg-white hover:text-[#525454]"
                             href="https://wa.me/+2349133354993"
                         >
                             Get Started
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,10 @@ const Nav = () => {
                 <div className="flex items-center justify-between">
                     <h4 className="font-semibold text-[0.99rem] text-[#525454]">
                         Treasure 👨‍🍳
-                    </h4>
+                    </h4>{" "}
                     <a
                         target="_blank"
-                        className="bg-[#525454] text-white px-3 py-2 text-[0.9rem] font-semibold rounded-lg border border-1 border-[#525454] hover:bg-white text-[#525454]"
+                        className="bg-[#525454] text-white px-3 py-2 text-[0.9rem] font-semibold rounded-lg border border-1 border-[#525454] hover:bg-white hover:text-[#525454]"
                         href="https://wa.me/+2349133354993"
                     >
                         Get Started
